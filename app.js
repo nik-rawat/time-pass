01ea651c27cc2e56f21c43ce641756565c639bf7
nik-rawat <nr2192004@gmail.com>
2025-01-11 23:42:14 +0530

    added

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const home = fs.readFileSync(path.join(__dirname, 'index.html'));
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        // res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(home);
     }
     if (req.url === '/about') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        // res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>About Page</h1>');
     }
     if (req.url === '/contact') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        // res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('<h1>Contact Page</h1>');
     }
 
-    res.writeHead(404, {'Content-Type': 'text/html'});
+    // res.writeHead(404, {'Content-Type': 'text/html'});
     res.end('<h1>Page Not Found</h1>');
 });
 
