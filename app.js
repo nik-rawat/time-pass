8e22fefadf55ddc0684d3a2a62658b5f5678c489
nik-rawat <nr2192004@gmail.com>
2025-01-11 23:40:34 +0530

    added

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ const PORT = process.env.PORT || 5000;
 const home = fs.readFileSync(path.join(__dirname, 'index.html'));
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(home);
     }
-);
+    if (req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>About Page</h1>');
+    }
+    if (req.url === '/contact') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Contact Page</h1>');
+    }
+
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end('<h1>Page Not Found</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
